869b617b32275c931b0e55e664c0502c0cc08f7c
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-12 19:16:18 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -14,6 +14,7 @@ class MusicLibraryController
     puts "To list all of the songs by a particular artist, enter 'list artist'."
     puts "To list all of the songs of a particular genre, enter 'list genre'."
     puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
     puts 
   end 
 end 
