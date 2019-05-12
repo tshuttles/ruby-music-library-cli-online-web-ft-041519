b50d8d80d6197ff1000af40fa4ef350937d87f8b
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-12 22:41:53 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -71,7 +71,7 @@ class MusicLibraryController
   
   def play_song
     puts "Which song number would you like to play?"
-    song_number = gets.strip 
+    song_number = gets.strip.to_i  
     Song.all 
     puts "Playing #{song.name} by #{song.artist.name}"
   end 
