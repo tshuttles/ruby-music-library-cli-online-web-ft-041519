b56239e8dd82c20fc8c045ad135aa88c08b508d7
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-12 19:19:00 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,7 +17,8 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-    until input = "exit" 
-      input 
+      until input = "exit" 
+        input = gets.strip 
+        
   end 
 end 
