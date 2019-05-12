257899392f16c0dad84cafbadaf5da1a4a90ea02
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-12 21:59:14 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -40,6 +40,6 @@ class MusicLibraryController
   
   def list_songs 
     song_list = Song.all.sort_by {|song| song.name} 
-    song_list.each_with_index(1) {|song, index| puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+    song_list.each.with_index(1) {|song, index| puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
   end 
 end 
