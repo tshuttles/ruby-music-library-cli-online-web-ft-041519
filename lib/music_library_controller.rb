1041dd26b35691ee221990549a76f2a5d2e14659
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-12 19:17:31 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,6 +7,7 @@ class MusicLibraryController
   end 
   
   def call 
+    input = ""
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
