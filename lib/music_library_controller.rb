ccbd67932b70ab5cbd9a12d0dc601cb9c6e609db
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-12 22:11:02 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,7 +56,7 @@ class MusicLibraryController
   def list_songs_by_artist 
     puts "Please enter the name of an artist:"
     input = gets.strip 
-    if input == artist 
+    if input == "artist" 
     end 
   end 
 end 
