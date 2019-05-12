52e5b5b159599e3ecec104ea0b847778db1a2781
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-12 20:25:24 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,6 +17,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
+      
       until input == "exit" 
         input = gets.strip 
         if input == "list songs" 
@@ -31,7 +32,7 @@ class MusicLibraryController
           list_genre
         elsif input == "play song" 
           play_song 
-        else 
+        end 
           
   end 
 end 
