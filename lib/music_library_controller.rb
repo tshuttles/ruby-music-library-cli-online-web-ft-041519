9fa519013cc330a860783206d243ab1a02a3b56f
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-12 19:16:06 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -13,6 +13,7 @@ class MusicLibraryController
     puts "To list all of the genres in your library, enter 'list genres'."
     puts "To list all of the songs by a particular artist, enter 'list artist'."
     puts "To list all of the songs of a particular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
     puts 
   end 
 end 
