57a2ef302716f30a239f98e933ebd7c8f1598612
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-13 00:11:23 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -74,6 +74,7 @@ class MusicLibraryController
     song_number = gets.strip.to_i  
     songs = Song.all 
     if (1..songs.length).include?(song_number)
+      song_number = 
     end 
     # puts "Playing #{song.name} by #{song.artist.name}"
   end 
